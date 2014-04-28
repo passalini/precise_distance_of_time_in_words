9918a8ae2fd43140ebd459c2e5ce9a160ad6d305
Toxix <toxix@server.fake>
2014-04-28 14:45:23 +0200

    I18n Support and some interface improvments
    fix: https://github.com/henrique2010/precise_distance_of_time_in_words/issues/2
    
    Also this contains a fix to make sure that the time distence is always
    positive and make the second parameter optional, so you can call it like
    precise_distance_of_time_in_words(12) # => "12 Seconds"
    
    If you call it with objects that response to_time this method is beeing
    used.

--- lib/precise_distance_of_time_in_words.rb
+++ lib/precise_distance_of_time_in_words.rb
@@ -1,7 +1,18 @@
 require "precise_distance_of_time_in_words/version"
 
 module PreciseDistanceOfTimeInWords
-  def precise_distance_of_time_in_words(from_time, to_time)
+  def precise_distance_of_time_in_words(from_time, to_time=0, options = {})
+    options = {
+      scope: :'datetime.precise_distance_in_words'
+    }.merge!(options)
+
+    # using to_time casting method of objects when presented
+    from_time = from_time.to_time if from_time.respond_to?(:to_time)
+    to_time = to_time.to_time if to_time.respond_to?(:to_time)
+
+    # make sure distance is positive
+    from_time, to_time = to_time, from_time if from_time > to_time
+
     dist = (to_time - from_time).round
 
     hours = dist / 3600
@@ -10,22 +21,22 @@ module PreciseDistanceOfTimeInWords
 
     words = ''
 
-    words << "#{hours} #{hours > 1 ? 'hours' : 'hour' }" if hours > 0
-    add_and_between?(words, hours, minutes)
-    words << "#{minutes} #{minutes > 1 ? 'minutes' : 'minute' }" if minutes > 0
+    I18n.with_options locale: options[:locale], scope: options[:scope] do |locale|
+      words << locale.t(:x_hours, count: hours) if hours > 0
+      words << " #{locale.t(:and)} " if (hours > 0 && minutes > 0) # add_and_between?
+      words << locale.t(:x_minutes, count: minutes) if minutes > 0
 
-    if seconds > 0 && (minutes <= 0 && hours <= 0)
-      words << "#{seconds} #{seconds > 1 ? 'seconds' : 'second' }"
+      if seconds > 0 && (minutes <= 0 && hours <= 0)
+        words << locale.t(:x_seconds, count: seconds)
+      end
     end
 
     return words
   end
-
-  private
-
-  def add_and_between?(words, time_1, time_2)
-    words << ' and ' if (time_1 > 0 && time_2 > 0)
-  end
 end
 
 include PreciseDistanceOfTimeInWords
+
+Dir["#{File.dirname(__FILE__)}/precise_distance_of_time_in_words/locales/*.yml"].each do |file|
+  I18n.load_path << file
+end
